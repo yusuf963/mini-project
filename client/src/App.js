4f2d8d3a38b29a0dae1795c183456e9313192634
ezio.intravaia <ezio.intravaia.radically.digital>
2022-02-24 17:11:26 +0000

    page completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,7 @@
-import "./App.css";
+import "./Sign-in.css";
+import googleIcon from "./assets/icons8-google-50.png";
+import line from "./assets/Line.svg";
+import radLogo from "./assets/logo.svg";
 
 import { auth, provider } from "./firebase";
 import { signInWithPopup } from "firebase/auth";
@@ -10,17 +13,25 @@ function App() {
         const user = result.user;
 
         console.log(user);
-        // ...
+
+        // to replace with "navigate" or anything similar with some router
+        window.location.href = "/home";
       })
       .catch((error) => {
         alert(error.message);
       });
 
   return (
+    // <Router>
     <div className="App">
-      <header className="App-header">Make your input</header>
-      <button onClick={() => signInWithGoogle()}>signIn</button>
+      <img className="radLogo" src={radLogo} alt="rad logo" />
+      <img className="radLogo" src={line} alt="line" />
+      <button className="signInButton" onClick={() => signInWithGoogle()}>
+        <img src={googleIcon} alt="sign in with google icon" />
+        Sign in with google
+      </button>
     </div>
+    // </Router>
   );
 }
 
