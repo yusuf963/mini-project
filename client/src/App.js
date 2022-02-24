811872fb159c6d1c349ef3fbcf6ca8f2f9e0df9f
ezio.intravaia <ezio.intravaia.radically.digital>
2022-02-24 15:09:20 +0000

    implemented logIn pop-up

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
+import "./App.css";
 
-import './App.css';
+import { auth, provider } from "./firebase";
+import { signInWithPopup } from "firebase/auth";
 
 function App() {
+  const signInWithGoogle = () =>
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user;
+
+        console.log(user);
+        // ...
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+
   return (
     <div className="App">
-      <header className="App-header">
-
-          Make your input
-      </header>
+      <header className="App-header">Make your input</header>
+      <button onClick={() => signInWithGoogle()}>signIn</button>
     </div>
   );
 }
