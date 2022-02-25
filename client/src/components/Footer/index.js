1d701569527fdecf2b715ae6e1ad0cee7d384b18
yusuf963 <mohammad963yusuf@gmail.com>
2022-02-25 11:22:30 +0000

    build RAD-alike footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,7 +1,35 @@
 import "./styles.footer.css";
+import logo from "../../assets/logo.svg";
+import behance_icon from "../../assets/Group-1.svg"
+import insta_icon from "../../assets/Group-2.svg"
+import linkedin_icon from "../../assets/Group-3.svg"
+import facebook_icon from "../../assets/Group-4.svg"
+import dribble_icon from "../../assets/Group-5.svg"
 
 export const Footer = () => {
   return (
-    <footer>FOOTER</footer>
+    <footer>
+      <div className="section-1">
+        <h5>RAD London</h5>
+        <p>145 City Rd, Hoxton</p>
+        <p>London, EC1V 1AZ</p>
+      </div>
+      <div className="section-2">
+        <h5>RAD Lisbon</h5>
+        <p>Heden Santa Apolónia,</p>
+        <p>Lisbon, 1100-651 </p>
+      </div>
+      <div className="section-3">
+        <img src={behance_icon} alt="behance-icon" />
+        <img src={insta_icon} alt="insta-icon" />
+        <img src={linkedin_icon} alt="linkedin-icon" />
+        <img src={facebook_icon} alt="facebook-icon" />
+        <img src={dribble_icon} alt="dribble-icon" />
+      </div>
+      <div className="section-4">
+        <img className="footer__logo" src={logo} alt="Rad logo" />
+        <p className="copy_right">&copy; 2022 Radically Digital - All Rights Reserved </p>
+      </div>
+    </footer>
   );
 };
