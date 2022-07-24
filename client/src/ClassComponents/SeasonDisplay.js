ef9cdd497ceebcf2d7c143a4123deb2168bbbe17
yusuf963 <mohammad963yusuf@gmail.com>
2022-07-24 11:15:23 +0100

    add styling

--- client/src/ClassComponents/SeasonDisplay.js
+++ client/src/ClassComponents/SeasonDisplay.js
@@ -1,12 +1,40 @@
 import React from 'react'
+import './style.season.css'
 class SeasonDisplay extends React.Component {
+  state = { currentMonth: null, season: null }
+
+
   render() {
-    console.log(this.props)
+    const seasonConfig = {
+      summer: {
+        text: "lets hit the beach",
+        iconName: 'sun'
+
+      },
+      winter: {
+        text: "its cold",
+        iconName: "star"
+
+      }
+    }
+    const getSeson = (lat, month) => {
+      if (month > 5 && month < 9) {
+        return lat > 0 ? "summer" : "winter"
+      } else {
+        return lat < 0 ? "winter" : "summer"
+      }
+    }
+    const season = getSeson(this.props.lat, new Date().getMonth())
+    //  change this into this
+    // const text = season === "winter" ? "Its Cold" : "Very Hot"
+    const { text, iconName } = seasonConfig[season]
     return (
-      <div>
+      <div className={`season-display ${season}`}>
+        <i className={`icon-left massive ${iconName} icon`}></i>
         <p>hello from season display</p>
-        <p>The season is</p>
+        <p>The season is {season}, {text}</p>
         <p>Lat is: {this.props.lat}</p>
+        <i className={`icon-right massive ${iconName} icon`}></i>
       </div>
     )
   }
